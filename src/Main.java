6dc5fc48bf06b4b6bc983a7187403b2119ca53c0
Unknown <alejimenezc@hotmail.com>
2019-09-05 21:06:01 -0600

    Creacion del scene

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-public class Main extends Application {
+public class Main{
     public static void main(String[] args){
-        launch(args);
-
-    }
-    public void start(Stage Stage1){
-        Stage1.setTitle("Circuit Designer");
-        StackPane layout = new StackPane();
-        Scene Pantalla = new Scene(layout,1300, 600);
-        Stage1.setScene(Pantalla);
-        Stage1.show();
 
     }
 }
