d63d1174eee3e721559345ea18c02b4170b783e6
Unknown <alejimenezc@hotmail.com>
2019-09-09 13:47:16 -0600

    Ajustes menores

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+
 public class Main{
     public static void main(String[] args){
 
