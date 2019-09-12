c0f7b82eec0a9daa13b7ecff6cf43fbaecf35b36
Unknown <alejimenezc@hotmail.com>
2019-09-12 00:17:17 -0600

    Reubicacion general y renombramiento de archivos

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,21 @@
 package sample;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class Controller {
 
+    private ImageView image1;
+    private ImageView image2;
+    private ImageView image3;
+    private ImageView image4;
+    private ImageView image5;
+    private ImageView image6;
+    private ImageView image7;
 
+    private void Mostrar(){
+        Image imagen1 = new Image("/400px-XOR_ANSI.svg.png");
+        Image imagen2 = new Image("/and_1.png");
+        Image imagen3 = new Image("/")
+    }
 }
