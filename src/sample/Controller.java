3943a103cf95cba8c0e4cd717420af113480e67e
Unknown <alejimenezc@hotmail.com>
2019-09-13 18:24:50 -0600

    Lista la paleta

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,21 +1,41 @@
 package sample;
 
+import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class Controller {
 
+    @FXML
     private ImageView image1;
+
+    @FXML
     private ImageView image2;
+
+    @FXML
     private ImageView image3;
+
+    @FXML
     private ImageView image4;
+
+    @FXML
     private ImageView image5;
+
+    @FXML
     private ImageView image6;
+
+    @FXML
     private ImageView image7;
 
-    private void Mostrar(){
-        Image imagen1 = new Image("/400px-XOR_ANSI.svg.png");
-        Image imagen2 = new Image("/and_1.png");
-        Image imagen3 = new Image("/")
+    public void Mostrar(){
+        image1.setImage(new Image("/AND.png"));
+        image2.setImage(new Image("/NAND.png"));
+        image3.setImage(new Image("/NOR.png"));
+        image4.setImage(new Image("/NOT.png"));
+        image5.setImage(new Image("/OR.png"));
+        image6.setImage(new Image("/XNOR.png"));
+        image7.setImage(new Image("/XOR.png"));
+
     }
 }
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Pantalla.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("Pantalla.fxml"));
+        Parent root = fxmlLoader.load();
+        Controller controller = (Controller) fxmlLoader.getController();
+        controller.Mostrar();
+
         primaryStage.setTitle("My New Circuit");
         primaryStage.setScene(new Scene(root, 1000, 700));
         primaryStage.show();
