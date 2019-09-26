020d1c1e11673b6e05d6f8d635ef42674fb58808
Unknown <alejimenezc@hotmail.com>
2019-09-25 23:45:12 -0600

    ?3

--- src/sample/NOT.java
+++ src/sample/NOT.java
@@ -4,4 +4,5 @@ public class NOT extends Compuerta {//Clase para la compuerta Not que hereda de 
     public NOT(boolean ValorV, Compuerta n, ListaPrevs p) {// Constructor
         super(ValorV, n, p);
     }
+    boolean prev;
 }
